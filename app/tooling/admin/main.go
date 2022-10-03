ce8ffd96a3bbe284c9c72028bca9576a63365186
Federico <s0rc3r3r01@users.noreply.github.com>
2022-10-03 17:40:14 +0100

    adding seeding

--- app/tooling/admin/main.go
+++ app/tooling/admin/main.go
@@ -23,9 +23,26 @@ func main() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-
 }
 
+// Seed loads test data into the database.
+func Seed(cfg database.Config) error {
+	db, err := database.Open(cfg)
+	if err != nil {
+		return fmt.Errorf("connect database: %w", err)
+	}
+	defer db.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := schema.Seed(ctx, db); err != nil {
+		return fmt.Errorf("seed database: %w", err)
+	}
+
+	fmt.Println("seed data complete")
+	return nil
+}
 func migrate() error {
 	cfg := database.Config{
 		User:         "postgres",
@@ -51,7 +68,8 @@ func migrate() error {
 	}
 
 	fmt.Println("migrations complete")
-	return nil
+
+	return Seed(cfg)
 }
 func genToken() error {
 
