d5e7b0717c9706b60646c8ecd9f087c51928b202
Federico <s0rc3r3r01@users.noreply.github.com>
2022-10-16 12:32:09 +0100

    query

--- business/data/store/user/user.go
+++ business/data/store/user/user.go
@@ -2,8 +2,16 @@
 package user
 
 import (
+	"context"
+	"fmt"
+	"time"
+
 	"github.com/jmoiron/sqlx"
+	"github.com/s0rc3r3r01/sampleService/business/auth"
+	"github.com/s0rc3r3r01/sampleService/business/sys/database"
+	"github.com/s0rc3r3r01/sampleService/business/sys/validate"
 	"go.uber.org/zap"
+	"golang.org/x/crypto/bcrypt"
 )
 
 // Store manages the set of API's for user access.
@@ -18,4 +26,222 @@ func NewStore(log *zap.SugaredLogger, db *sqlx.DB) Store {
 		log: log,
 		db:  db,
 	}
+}
+
+func (s Store) Create(ctx context.Context, nu NewUser, now time.Time) (User, error) {
+	if err := validate.Check(nu); err != nil {
+		return User{}, fmt.Errorf("validating data: %w", err)
+	}
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(nu.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return User{}, fmt.Errorf("generating password hash: %w", err)
+	}
+
+	usr := User{
+		ID:           validate.GenerateID(),
+		Name:         nu.Name,
+		Email:        nu.Email,
+		PasswordHash: hash,
+		Roles:        nu.Roles,
+		DateCreated:  now,
+		DateUpdated:  now,
+	}
+
+	const q = `
+	INSERT INTO users
+		(user_id, name, email, password_hash, roles, date_created, date_updated)
+	VALUES
+		(:user_id, :name, :email, :password_hash, :roles, :date_created, :date_updated)`
+
+	if err := database.NamedExecContext(ctx, s.log, s.db, q, usr); err != nil {
+		return User{}, fmt.Errorf("inserting user: %w", err)
+	}
+
+	return usr, nil
+}
+
+// Update replaces a user document in the database.
+func (s Store) Update(ctx context.Context, claims auth.Claims, userID string, uu UpdateUser, now time.Time) error {
+	if err := validate.CheckID(userID); err != nil {
+		return database.ErrInvalidID
+	}
+	if err := validate.Check(uu); err != nil {
+		return fmt.Errorf("validating data: %w", err)
+	}
+
+	usr, err := s.QueryByID(ctx, claims, userID)
+	if err != nil {
+		return fmt.Errorf("updating user userID[%s]: %w", userID, err)
+	}
+
+	if uu.Name != nil {
+		usr.Name = *uu.Name
+	}
+	if uu.Email != nil {
+		usr.Email = *uu.Email
+	}
+	if uu.Roles != nil {
+		usr.Roles = uu.Roles
+	}
+	if uu.Password != nil {
+		pw, err := bcrypt.GenerateFromPassword([]byte(*uu.Password), bcrypt.DefaultCost)
+		if err != nil {
+			return fmt.Errorf("generating password hash: %w", err)
+		}
+		usr.PasswordHash = pw
+	}
+	usr.DateUpdated = now
+
+	const q = `
+	UPDATE
+		users
+	SET 
+		"name" = :name,
+		"email" = :email,
+		"roles" = :roles,
+		"password_hash" = :password_hash,
+		"date_updated" = :date_updated
+	WHERE
+		user_id = :user_id`
+
+	if err := database.NamedExecContext(ctx, s.log, s.db, q, usr); err != nil {
+		return fmt.Errorf("updating userID[%s]: %w", userID, err)
+	}
+
+	return nil
+}
+
+// Delete removes a user from the database.
+func (s Store) Delete(ctx context.Context, claims auth.Claims, userID string) error {
+	if err := validate.CheckID(userID); err != nil {
+		return database.ErrInvalidID
+	}
+
+	// If you are not an admin and looking to delete someone other than yourself.
+	if !claims.Authorized(auth.RoleAdmin) && claims.Subject != userID {
+		return database.ErrForbidden
+	}
+
+	data := struct {
+		UserID string `db:"user_id"`
+	}{
+		UserID: userID,
+	}
+
+	const q = `
+	DELETE FROM
+		users
+	WHERE
+		user_id = :user_id`
+
+	if err := database.NamedExecContext(ctx, s.log, s.db, q, data); err != nil {
+		return fmt.Errorf("deleting userID[%s]: %w", userID, err)
+	}
+
+	return nil
+}
+
+// Query retrieves a list of existing users from the database.
+func (s Store) Query(ctx context.Context, pageNumber int, rowsPerPage int) ([]User, error) {
+	data := struct {
+		Offset      int `db:"offset"`
+		RowsPerPage int `db:"rows_per_page"`
+	}{
+		Offset:      (pageNumber - 1) * rowsPerPage,
+		RowsPerPage: rowsPerPage,
+	}
+
+	const q = `
+	SELECT
+		*
+	FROM
+		users
+	ORDER BY
+		user_id
+	OFFSET :offset ROWS FETCH NEXT :rows_per_page ROWS ONLY`
+
+	var users []User
+	if err := database.NamedQuerySlice(ctx, s.log, s.db, q, data, &users); err != nil {
+		if err == database.ErrNotFound {
+			return nil, database.ErrNotFound
+		}
+		return nil, fmt.Errorf("selecting users: %w", err)
+	}
+
+	return users, nil
+}
+
+// QueryByID gets the specified user from the database.
+func (s Store) QueryByID(ctx context.Context, claims auth.Claims, userID string) (User, error) {
+	if err := validate.CheckID(userID); err != nil {
+		return User{}, database.ErrInvalidID
+	}
+
+	// If you are not an admin and looking to retrieve someone other than yourself.
+	if !claims.Authorized(auth.RoleAdmin) && claims.Subject != userID {
+		return User{}, database.ErrForbidden
+	}
+
+	data := struct {
+		UserID string `db:"user_id"`
+	}{
+		UserID: userID,
+	}
+
+	const q = `
+	SELECT
+		*
+	FROM
+		users
+	WHERE 
+		user_id = :user_id`
+
+	var usr User
+	if err := database.NamedQueryStruct(ctx, s.log, s.db, q, data, &usr); err != nil {
+		if err == database.ErrNotFound {
+			return User{}, database.ErrNotFound
+		}
+		return User{}, fmt.Errorf("selecting userID[%q]: %w", userID, err)
+	}
+
+	return usr, nil
+}
+
+// QueryByEmail gets the specified user from the database by email.
+func (s Store) QueryByEmail(ctx context.Context, claims auth.Claims, email string) (User, error) {
+
+	// Add Email Validate function in validate
+	// if err := validate.Email(email); err != nil {
+	// 	return User{}, ErrInvalidEmail
+	// }
+
+	data := struct {
+		Email string `db:"email"`
+	}{
+		Email: email,
+	}
+
+	const q = `
+	SELECT
+		*
+	FROM
+		users
+	WHERE
+		email = :email`
+
+	var usr User
+	if err := database.NamedQueryStruct(ctx, s.log, s.db, q, data, &usr); err != nil {
+		if err == database.ErrNotFound {
+			return User{}, database.ErrNotFound
+		}
+		return User{}, fmt.Errorf("selecting email[%q]: %w", email, err)
+	}
+
+	// If you are not an admin and looking to retrieve someone other than yourself.
+	if !claims.Authorized(auth.RoleAdmin) && claims.Subject != usr.ID {
+		return User{}, database.ErrForbidden
+	}
+
+	return usr, nil
 }
