fe4190fcc23ae5a89dc43718cdf9bd9c5faaf4ab
Federico <s0rc3r3r01@users.noreply.github.com>
2022-10-16 15:38:39 +0100

    adding APIs

--- app/services/sales-api/handlers/handlers.go
+++ app/services/sales-api/handlers/handlers.go
@@ -10,7 +10,9 @@ import (
 	"github.com/jmoiron/sqlx"
 	"github.com/s0rc3r3r01/sampleService/app/services/sales-api/handlers/debug/checkgrp"
 	"github.com/s0rc3r3r01/sampleService/app/services/sales-api/handlers/v1/testgrp"
+	"github.com/s0rc3r3r01/sampleService/app/services/sales-api/handlers/v1/usergrp"
 	"github.com/s0rc3r3r01/sampleService/business/auth"
+	"github.com/s0rc3r3r01/sampleService/business/core/user"
 	"github.com/s0rc3r3r01/sampleService/business/web/mid"
 	"github.com/s0rc3r3r01/sampleService/foundation/web"
 
@@ -59,8 +61,20 @@ func v1(app *web.App, cfg APIMuxConfig) {
 	tgh := testgrp.Handlers{
 		Log: cfg.Log,
 	}
+	// Register user management and authentication endpoints.
+	ugh := usergrp.Handlers{
+		User: user.NewCore(cfg.Log, cfg.DB),
+		Auth: cfg.Auth,
+	}
 	app.Handle(http.MethodGet, version, "/test", tgh.Test)
 	app.Handle(http.MethodGet, version, "/testauth", tgh.Test, mid.Authenticate(cfg.Auth), mid.Authorize(("ADMIN")))
+
+	app.Handle(http.MethodGet, version, "/users/token", ugh.Token)
+	app.Handle(http.MethodGet, version, "/users/:page/:rows", ugh.Query, mid.Authenticate(cfg.Auth), mid.Authorize(auth.RoleAdmin))
+	app.Handle(http.MethodGet, version, "/users/:id", ugh.QueryByID, mid.Authenticate(cfg.Auth))
+	app.Handle(http.MethodPost, version, "/users", ugh.Create, mid.Authenticate(cfg.Auth), mid.Authorize(auth.RoleAdmin))
+	app.Handle(http.MethodPut, version, "/users/:id", ugh.Update, mid.Authenticate(cfg.Auth), mid.Authorize(auth.RoleAdmin))
+	app.Handle(http.MethodDelete, version, "/users/:id", ugh.Delete, mid.Authenticate(cfg.Auth), mid.Authorize(auth.RoleAdmin))
 
 }
 
